4376f4d2ea04b96a8c63746c3b8edce27e588a83
Caseyf556 <154101111+Caseyf556@users.noreply.github.com>
2026-02-23 13:07:48 -0600

    Add DatabaseConnection class for SQLite connection

---
+++ files/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package contacts;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String DB_URL = "jdbc:sqlite:contacts.db";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+}
